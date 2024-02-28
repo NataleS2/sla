65382eeff8fb6aec81a028a9a7181757bbf67e48
NatyS2 <125456026+NataleS2@users.noreply.github.com>
2024-02-28 17:59:36 -0300

    Update prova.js
    exemplo de prova usando js

--- prova.js
+++ prova.js
@@ -41,3 +41,27 @@ function validarCadastro() {
 }
 //valida o cadastro.
 validarCadastro()
+
+//Objeto aluno1: Primeiro, um objeto JavaScript chamado aluno1 é definido. Este objeto contém várias informações sobre um aluno, como nome, idade, disciplinas cursadas, 
+//datas de presença e seu registro acadêmico (RA).
+
+//Função validarCadastro(): Esta função é responsável por validar o cadastro do aluno com base em certos critérios.
+//Validação do Nome: A primeira verificação na função é se o nome do aluno (aluno1.nome) tem menos de 10 caracteres. Se for o caso,
+//a função exibe "Nome inválido" no console e retorna, encerrando a execução da função.
+
+//Validação da Idade: Em seguida, verifica-se se a idade do aluno (aluno1.idade) é menor que 14 anos. Se for o caso,
+//a função exibe "Idade inválida" no console e retorna.
+
+//Validação das Disciplinas Cursadas: A função verifica se o aluno está cursando menos de três disciplinas (aluno1.cursando.length). 
+//Se estiver, exibe "Falta de Matéria" e retorna.
+
+//Validação da Presença: A próxima verificação é para garantir que o aluno tenha pelo menos duas datas registradas de presença (aluno1.presença.length).
+//Se não, exibe "Falta de Presença" e retorna.
+
+//Validação do Registro Acadêmico (RA): Por fim, é verificado se o registro acadêmico (RA) do aluno (aluno1.ra.length) tem menos de sete caracteres. 
+//Se sim, exibe "RA inválido" e retorna.
+
+//Impressão dos Detalhes do Aluno: Se todas as verificações forem bem-sucedidas, ou seja, se o aluno atender a todos os critérios de validação, 
+//os detalhes do aluno são impressos no console usando console.log(aluno1).
+
+//Chamada da Função validarCadastro(): Por fim, a função validarCadastro() é chamada para iniciar o processo de validação do cadastro do aluno aluno1.
